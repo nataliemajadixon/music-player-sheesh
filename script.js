a7e633e0be5cc08668e189f91b884f6d1f22e048
natolay <ndixon@seattleu.edu>
2022-10-16 19:51:10 +0200

    added functionality to next and previous buttons

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ const cover = document.querySelector('#cover')
 
 
 
-const songs = ['bodylang', 'typical', 'makeit']
+const songs = ['Body Language', 'Typical', 'Make It']
 
-let songIndex = 1
+let songIndex = 2
 
 loadSong(songs[songIndex])
 
 function loadSong(song) {
     title.innerHTML = song
     audio.src = `songs/${song}.mp3`
-    audio.src = `song-cover/${song}.jpg`
+    audio.src = `images/${song}.jpg`
 }
 function playSong() {
 musicContainer.classList.add('play')
@@ -37,9 +37,43 @@ playBtn.querySelector('i.fas').classList.remove('fa-pause')
     audio.pause()
 }
 
+function prevSong() {
+    songIndex--
 
+    if(songIndex > songs.length - 1) {
+        songIndex = 0
+    }
 
+    loadSong(songs[songIndex])
 
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
 
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play')
@@ -52,3 +86,11 @@ playBtn.addEventListener('click', () => {
 })
 
 
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
